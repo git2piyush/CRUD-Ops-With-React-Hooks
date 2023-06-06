b316ede6077efd95f59ba600e2ad44b6cd7e62f7
Piyush Soni <130133404+git2piyush@users.noreply.github.com>
2023-06-06 13:20:21 +0530

    Initial commit

---
+++ src/tables/UserTable.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+const UserTable = props => (
+const [editData,setEditData] = useState({id:null, name:"", username:""})
+const handleEditDataChange = (e)=>{
+  setEditData({...editData,[e.target.name]: e.target.value})
+    
+  }
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Username</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {props.users.length > 0 ? (
+        props.users.map(user => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>
+              <button className="button muted-button" 
+               onClick={() => props.editUser(user.id)}>Edit</button>
+              <button
+                className="button muted-button"
+                onClick={() => props.deleteUser(user.id)}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={3}>No users</td>
+        </tr>
+      )}
+    </tbody>
+  </table>
+);
+
+export default UserTable;
